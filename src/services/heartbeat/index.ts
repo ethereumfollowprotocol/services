2c743d01a5cc9f61bac2d97305db12f18871b987
0xthrpw <0xthrpw@gmail.com>
2025-01-27 22:13:34 -0500

    check

--- src/services/heartbeat/index.ts
+++ src/services/heartbeat/index.ts
@@ -2,12 +2,16 @@ import { env } from "#/env";
 import { logger } from "#/logger";
 
 export async function sendHeartbeat() {
-	try {
-		const response = await fetch(`${env.HEARTBEAT_URL}`);
-        const result = await response.text();
-		logger.log("Heartbeat registered");
-	} catch (error) {
-		const errorMessage = error instanceof Error ? error.message : error;
-		logger.error("EXCEPTION", errorMessage);
-	}
+    if(env.HEARTBEAT_URL !== "unset"){
+        try {
+            const response = await fetch(`${env.HEARTBEAT_URL}`);
+            const result = await response.text();
+            logger.log("Heartbeat registered");
+        } catch (error) {
+            const errorMessage = error instanceof Error ? error.message : error;
+            logger.error("EXCEPTION", errorMessage);
+        }
+    }else{
+        logger.warn("Heartbeat URL not set");
+    }
 }
