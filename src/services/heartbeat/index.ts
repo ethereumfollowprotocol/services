581b8dc4f2b8c5335418460990cd7d5013e66e0d
0xthrpw <0xthrpw@gmail.com>
2025-01-27 22:09:48 -0500

    heartbeat url

--- src/services/heartbeat/index.ts
+++ src/services/heartbeat/index.ts
@@ -3,7 +3,8 @@ import { logger } from "#/logger";
 
 export async function sendHeartbeat() {
 	try {
-		const response = await fetch(`https://nosnch.in/${env.SNITCH_ID}`);
+		const response = await fetch(`${env.HEARTBEAT_URL}`);
+        const result = await response.text();
 		logger.log("Heartbeat registered");
 	} catch (error) {
 		const errorMessage = error instanceof Error ? error.message : error;
